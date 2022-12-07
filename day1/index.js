a1a3c1f24a2107d37787c135141b9f4d0689c8af
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-12-06 20:25:00 -0800

    did part 2

--- day1/index.js
+++ day1/index.js
@@ -10,12 +10,13 @@ const how_many_cals = async () => {
       input: fs.createReadStream(TEXT_FILE),
       crlfDelay: Infinity,
     });
+    let max_arr = [];
     let max = 0;
     let curr = 0;
     rl.on("line", (line) => {
-      console.log(line);
       if (line == "") {
         max = Math.max(curr, max);
+        max_arr.push(curr);
         console.log(curr);
         curr = 0;
       }
@@ -23,6 +24,16 @@ const how_many_cals = async () => {
     });
     await events.once(rl, "close");
     console.log(`The max calories was ${max}`);
+    console.log(
+      max_arr.sort((a, b) => {
+        return b - a;
+      })
+    );
+    let sum = 0;
+    for (let i = 0; i < 3; i++) {
+      sum += max_arr[i];
+    }
+    console.log(sum);
   } catch (error) {
     console.error(error);
   }
